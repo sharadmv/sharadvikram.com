9504f0cf81bd8e80e4affbd74d410df85c53841b
Sharad Vikram <sharad.vikram@gmail.com>
2016-02-10 22:05:52 -0800

    added ihbc

--- sv/static/js/controllers.js
+++ sv/static/js/controllers.js
@@ -1,12 +1,19 @@
 app.controller("ResearchController", function($scope) {
   $scope.papers = [
     {
+      title: "Interactive Bayesian Hierarchical Clustering",
+      authors: "Sharad Vikram and Sanjoy Dasgupta",
+      venue: "Arxiv",
+      link: "http://arxiv.org/abs/1602.03258",
+      year: 2015,
+    },
+    {
       title: "Interactive Hierarchical Clustering using Bayesian Nonparametrics",
       authors: "Sharad Vikram and Sanjoy Dasgupta",
       venue: "NIPS Workshop - Bayesian Nonparametrics: The Next Generation",
       link: "/pdf/research/ihcbn.pdf",
       year: 2015,
-    }
+    },
   ]
 });
 
