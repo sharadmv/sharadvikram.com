d37979772ada521274ca862764c875e7ad0962c3
Sharad Vikram <sharad.vikram@gmail.com>
2018-11-01 17:23:14 -0700

    updated papers

--- sv/static/js/controllers.js
+++ sv/static/js/controllers.js
@@ -1,11 +1,46 @@
 app.controller("ResearchController", function($scope) {
   $scope.papers = [
     {
+      title: "The LORACs prior for VAEs: Letting the Trees Speak for the Data",
+      authors: "Sharad Vikram, Matthew D Hoffman and Matthew J Johnson",
+      venue: "Preprint",
+      link: "https://arxiv.org/pdf/1810.06891",
+      year: 2018,
+    },
+    {
+      title: "SOLAR: Deep Structured Latent Representations for Model-Based Reinforcement Learning",
+      authors: "Marvin Zhang*, Sharad Vikram*, Laura Smith, Pieter Abbeel, Matthew J Johnson and Sergey Levine",
+      venue: "Preprint",
+      link: "https://arxiv.org/pdf/1808.09105",
+      year: 2018,
+    },
+    {
+      title: "Estimating Reactions and Recommending Products with Generative Models of Reviews",
+      authors: "Jianmo Ni, Zachary Lipton, Sharad Vikram and Julian McAuley",
+      venue: "International Joint Conference on Natural Language Processing",
+      link: "http://www.aclweb.org/anthology/I17-1079",
+      year: 2017,
+    },
+    {
+      title: "Estimating Reactions and Recommending Products with Generative Models of Reviews",
+      authors: "Jianmo Ni, Zachary Lipton, Sharad Vikram and Julian McAuley",
+      venue: "International Joint Conference on Natural Language Processing",
+      link: "http://www.aclweb.org/anthology/I17-1079",
+      year: 2017,
+    },
+    {
       title: "Interactive Bayesian Hierarchical Clustering",
       authors: "Sharad Vikram and Sanjoy Dasgupta",
       venue: "International Conference on Machine Learning",
       link: "http://arxiv.org/pdf/1602.03258",
       year: 2016,
+    },
+    {
+      title: "Generative Concatenative Nets Jointly Learn to Write and Classify Reviews",
+      authors: "Zachary Lipton, Sharad Vikram and Julian McAuley",
+      venue: "Preprint",
+      link: "https://arxiv.org/pdf/1511.03683",
+      year: 2015,
     },
     {
       title: "Interactive Hierarchical Clustering using Bayesian Nonparametrics",
